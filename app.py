d5ae56d98682b78ecf992221fa202270d691e4dd
Harman946 <harmankaurmakkad13@gmail.com>
2025-11-10 23:44:41 +0530

    fix the module not found error

--- app.py
+++ app.py
@@ -78,10 +78,10 @@ import joblib
 import numpy as np
 import re
 import pandas as pd
-import nltk
-nltk.download('stopwords') 
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
+import nltk
+nltk.download('stopwords') 
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
